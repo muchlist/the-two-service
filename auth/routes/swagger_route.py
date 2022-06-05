d61ecbe37135861853ff556828ab86e2dee54100
muchlis <muchlis@majoo.id>
2022-06-05 07:28:16 +0700

    fix(auth-routing): change swagger url
    change swagger url for auth api

--- auth/routes/swagger_route.py
+++ auth/routes/swagger_route.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('swagger_bp', __name__)
 
-@bp.route('/docs', methods=['GET'])
+@bp.route('/swagger', methods=['GET'])
 def get_docs():
     print('sending docs')
     return render_template('swaggerui.html')
